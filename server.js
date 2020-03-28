9bc450f2f90d4e4a5beec651f5d4803a4305f8e8
taypal <tay.houk@yahoo.com>
2020-03-28 14:58:11 -0600

    created server

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(morgan());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongod://localhost/workout", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.use(require("./routes/htmlroutes.js"));
+
+app.listen(PORT, () => {
+    console.log("App running on http://localhost:" + PORT);
+});
